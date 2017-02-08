0446b82b1cb225ca073d87f2a9843473ac2d5d94
Ricardo Perez <ricardop@square-enix.com>
2017-02-08 18:16:50 +0000

    Create Task migration

---
+++ priv/repo/migrations/20170119182230_create_task.exs
@@ -0,0 +1,16 @@
+defmodule TripPlanner.Repo.Migrations.CreateTask do
+  use Ecto.Migration
+
+  def change do
+    create table(:tasks) do
+      add :name, :string
+      add :desc, :string
+      add :start, Ecto.DateTime
+      add :finish, Ecto.DateTime
+      add :done, :boolean, default: false
+      add :location_lat, :float
+      add :location_long, :float
+      timestamps()
+    end
+  end
+end
